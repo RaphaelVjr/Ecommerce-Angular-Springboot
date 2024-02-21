a7bded15d713efec1b50fb6a93e8c6e51e4f883a
Raphael Vitorio <raphaelvitorio01@gmail.com>
2024-02-21 20:30:56 -0300

    setup features

---
+++ src/data/mainCarousel.ts
@@ -0,0 +1,14 @@
+export const homeCarouselData=[
+    {
+       image:"https://images.stockx.com/images/Nike-x-Stussy-T-Shirt-Black.jpg?fit=clip&bg=FFFFFF&auto=compress&q=90&dpr=2&trim=color&updated_at=1607708795&fm=jpg&ixlib=react-9.1.1&w=2619",
+       path:"/men/clothing/stussyxnike_tshirt"
+    },
+    {
+        image:"https://twicpics.celine.com/product-prd/images/large/2X764671Q.38AW_1_WIN21_W_V9.jpg?twic=v1/resize-max=2500/quality-min=100",
+        path:"/men/clothing/celineblack_tshirt"
+    },
+    {
+        image:"https://ns.imgix.net/product/1621515174599_FEAROFGODE0.octet-stream?fit=fill&bg=FFFFFF&trim=color&auto=format,compress&q=90",
+        path:"/men/clothing/fearofgod_essentials_tshirt"
+    },
+]
